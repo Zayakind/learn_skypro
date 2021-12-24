e9a12b90bd898e5e14cf8278db351e951a991adc
Zayakind <63289485+Zayakind@users.noreply.github.com>
2021-12-24 17:14:31 +0500

    Update directory

--- main.py
+++
@@ -1,7 +0,0 @@
-from homework21_labirint.controller import GameController
-
-if __name__ == '__main__':
-    gc = GameController()
-    levelstring = 'WWWWWWWWWW\nWggGgggggW\nWgTTTggTgW\nWgTgTggDgW\nWgTTTggTgW\nWggTggTTgW\nWgTTTgggTW\nWTgggTgTgW\nWKgTggTggW\nWWWWWWWWWW'
-    gc.make_field(levelstring)
-    gc.play()
